ccd87b149607d50f25d60fc2bf30914f44c50529
Legendz99 <Legendz99>
2016-11-19 21:53:11 +0700

    Indonesia Android Kejar Batch 2

---
+++ app/src/main/java/androidkejar/noteapp/listener/RecyclerItemClickListener.java
@@ -0,0 +1,15 @@
+package androidkejar.noteapp.listener;
+
+import android.view.View;
+
+/**
+ * Created by Dodi Rivaldi on 13/11/2016.
+ */
+
+public interface RecyclerItemClickListener {
+
+    void onItemClick(int position, View view);
+
+    void onItemSelected(int position, View view);
+
+}
